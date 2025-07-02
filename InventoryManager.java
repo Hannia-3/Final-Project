8921bbcb9c2de2aeb167fc1274ad8a3b37aca0dc
Hannia-3 <Hannia.EscPM@gmail.com>
2025-07-02 15:26:11 -0500

    Update and rename VinylInventoryManager.java to InventoryManager.java

---
+++ InventoryManager.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class InventoryManager {
+    private ArrayList<Record> availableRecords;
+    private Record[] soldOutRecords;
+
+    publicInventoryManager() {
+        availableRecords = new ArrayList<>();
+        soldOutRecords = new Record[50];
+    }
+
+    public void addRecord(Record record) {
+        availableRecord.add(record);
+    }
+
+    public void sellRecord(Record record){
--- VinylInventoryManager.java
+++
@@ -1,3 +0,0 @@
-public void addRecord(Record record) {
-    inStockRecords.add(record);
-}
