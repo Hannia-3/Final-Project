3b979c3e157f24ff7be10f1a413a7a548d4623c2
Hannia-3 <Hannia.EscPM@gmail.com>
2025-07-02 15:20:53 -0500

    Update Record.java

--- Record.java
+++ Record.java
@@ -1,5 +1,13 @@
 public class Record implements java.io.Serializable {
     private String title;
     private String artist;
-    private int quantity;
+    private double price;
+    private int stock;
+
+    public Record(String title, String artist, double price, int stock) {
+        this.title = title;
+        this.artist = artist;
+        this.price = price;
+        this.stock = stock;
+    }
 }
