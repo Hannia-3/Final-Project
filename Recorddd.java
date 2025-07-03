a5e79b68c9cc71296ee8e98e6a19f2f91d4a1800
Hannia-3 <Hannia.EscPM@gmail.com>
2025-07-02 20:18:32 -0500

    Update Recorddd.java

--- Recorddd.java
+++ Recorddd.java
@@ -16,8 +16,13 @@ public class Record implements Serializable {
     // Getters and Setters for all fields
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
-    // ... (similar for artist, genre, year, quantity)
-
+    public String getArtist() { return artist; }
+    public void setArtist(String artist) { this.artist = artist; }
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+    public int getQuantity() { return quantity; }
     public void setQuantity(int quantity) { this.quantity = quantity; }
 
     @Override
